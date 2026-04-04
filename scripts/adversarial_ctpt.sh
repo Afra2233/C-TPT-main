4f8a3bbfb73735a30522a1ade737a6a15d051bfa
afra818 <60651664+afra818@users.noreply.github.com>
2026-04-04 21:26:37 +0100

    f

--- scripts/adversarial_ctpt.sh
+++ scripts/adversarial_ctpt.sh
@@ -18,13 +18,15 @@ testsets=$1
 arch=ViT-B/32
 bs=64
 ctx_init=a_photo_of_a
-run_type=tpt_ctpt
-lambda_term=50
+# run_type=tpt_ctpt
+# lambda_term=50
+run_type=baseline
+lambda_term=0
 
 # PGD params
-pgd_eps=0.03137255      # 8/255
-pgd_alpha=0.00784314    # 2/255
-pgd_steps=10
+pgd_eps=0.01568627       # 4/255
+pgd_alpha=0.00392157 # 1/255
+pgd_steps=5
 robust_clip_ckpt=/scratch/hpc/07/zhang303/C-TPT-main/TeCoAmodel_best.pth.tar
 
 srun python ./adversarial_ctpt.py ${data_root} \
@@ -32,7 +34,6 @@ srun python ./adversarial_ctpt.py ${data_root} \
   -a ${arch} \
   -b ${bs} \
   --gpu 0 \
-  --tpt \
   --ctx_init ${ctx_init} \
   --run_type ${run_type} \
   --lambda_term ${lambda_term} \
@@ -42,3 +43,4 @@ srun python ./adversarial_ctpt.py ${data_root} \
   --pgd_steps ${pgd_steps} \
   --pgd_random_start \
   --robust_clip_ckpt ${robust_clip_ckpt}
+  # --tpt \
